aeb40f995aba442b112d7f15b8a86e7cd1ee633c
Samshiel <sellissander@gmail.com>
2021-10-20 13:52:00 +0300

    update

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+
 import movement
 import keyboard
 
 while(True):
-    arv = movement.setMovement(int(input()))
     if keyboard.is_pressed("q"):
         print("Stopped by keypress")
         movement.stop()
--- src/movement.py
+++ src/movement.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import math
 import serial
 
@@ -5,9 +7,9 @@ firstAngle = 0
 secondAngle = 120
 thirdAgnle = 240
 
-#ser = serial.Serial(port="/dev/ttyACM0",
-#           baudrate=115200,
-#           timeout = 2)
+ser = serial.Serial(port="/dev/ttyACM0",
+           baudrate=115200,
+           timeout = 2)
 
 def omniWheel(speed, angle, direction):
     vel = speed * math.cos(math.radians(direction - angle))
@@ -22,8 +24,8 @@ def setMovement(direction):
     z = int(omniWheel(speed, thirdAgnle, direction))
     text = ("<hhhHH",str(x), str(y), str(y), "0xAAAA")
 
-    #ser.write(text.encode("utf-8"))
+    ser.write(text.encode("utf-8"))
 
 def stop():
     print("STOP")
-    #ser.write("<hhhHH,0,0,0,0xAAAA".encode("utf-8"))
+    ser.write("<hhhHH,0,0,0,0xAAAA".encode("utf-8"))
