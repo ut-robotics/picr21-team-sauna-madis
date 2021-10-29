832dfe5bb1f0bac920d99ae7ba1876c3234f1ca1
urmo <u.schults@gmail.com>
2021-10-29 14:43:58 +0300

    startRobot threadide lisamine

--- saun/startRobot.py
+++ saun/startRobot.py
@@ -1,10 +1,10 @@
 import threading
-import main
+#import main
 import cameraImage
 
 if __name__ == "__main__":
     cameraThread = threading.Thread(target=cameraImage())
-    mainThread = threading.Thread(target=main())
+    #mainThread = threading.Thread(target=main())
 
     cameraThread.start()
     print("Alustasin cameraImage threadi")
