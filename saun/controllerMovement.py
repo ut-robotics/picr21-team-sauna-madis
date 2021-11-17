1a1787efb57382a7058983e6e70d8d7a04d1926c
urmo <u.schults@gmail.com>
2021-11-17 17:01:50 +0200

    pallini jõudmine

--- saun/controllerMovement.py
+++ saun/controllerMovement.py
@@ -4,12 +4,21 @@ import movement
 while True:
     try:
         if keyboard.is_pressed("w"):
+            print("ControllerEdasi")
             movement.forward()
         elif keyboard.is_pressed("s"):
+            print("stop")
             movement.stop()
         elif keyboard.is_pressed("a"):
+            print("ControllerVasakule")
             movement.turnLeft()
         elif keyboard.is_pressed("d"):
+            print("ControllerParemale")
             movement.turnRight()
+        
+        if keyboard.is_pressed("q"):
+            movement.stop()
+            print("Stopped by keypress")
+            break
     except:
         print("vale nupp")
