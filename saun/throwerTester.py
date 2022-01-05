041b66ee050d52e9e45634b1dc0a3ea1088f4d7e
Samshiel <sellissander@gmail.com>
2022-01-05 19:39:56 +0200

    throwerTest created

---
+++ saun/throwerTester.py
@@ -0,0 +1,77 @@
+import cameraImage
+import movement
+from pyPS4Controller.controller import Controller
+from threading import Thread
+
+##              INFO            ##
+# X to save
+# [] to stop thrower motor
+# O to start thrower speed
+# Left & Right arrows to set speed steps
+# UP & Down arrows to set speed
+##################################
+
+distance = 0
+throwerspeed = 0
+steps = 10
+
+def save():
+    with open("throwerData.txt", "w") as file:
+        file.write(throwerspeed + ";" + distace + "\n")
+
+class controller:
+
+    def __init__(self):
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.listen, args=()).start()
+        return self
+
+    def listen(self):
+
+        class MyController(Controller):
+
+            def __init__(self, **kwargs):
+                Controller.__init__(self, **kwargs)
+
+            def on_x_press(self):
+                save()
+
+            def on_circle_press(self):
+                movement.thrower(throwerspeed)
+
+            def on_square_press(self):
+                movement.stop()
+
+            def on_up_arrow_press(self):
+                global throwerspeed
+                throwerspeed = throwerspeed + steps
+                print(throwerspeed)
+
+            def on_down_arrow_press(self):
+                global throwerspeed
+                throwerspeed = throwerspeed - steps
+                print(throwerspeed)
+
+            def on_left_arrow_press(self):
+                if (steps > 1):
+                    steps / 10
+
+            def on_right_arrow_press(self):
+                if(steps < 100):
+                    steps * 10
+
+            def on_circle_press(self):
+                movement.thrower(throwerspeed)
+
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen(timeout=60)
+
+while True:
+    cameraImage.get_image("Blue")
+    distace = cameraImage.getDepth()
+    print("Steps: " + steps + ";  Speed: " + throwerspeed + ";  Distance: " + distace)
+
+
+
