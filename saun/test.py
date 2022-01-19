075eeb2f01c5fa439b84ab0dd83d923bf2003176
Samshiel <sellissander@gmail.com>
2022-01-19 15:53:05 +0200

    image update 1.1

--- saun/test.py
+++ saun/test.py
@@ -2,7 +2,7 @@ from image import *
 from imageProcess import *
 
 pilt = image()
-pilditootlusPall = imageProcess(50, 9999999, "ball", True)
+pilditootlusPall = imageProcess(50, 9999999, "ball")
 
 while True:
     pilditootlusPall.find_objects(pilt.get_rbg_image())
