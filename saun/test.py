c9100ffc4025b568c5c3716e103b3efce6bcf720
urmo <u.schults@gmail.com>
2022-01-20 19:37:46 +0200

    test

--- saun/test.py
+++ saun/test.py
@@ -1,10 +1,12 @@
 from image import *
 from imageProcess import *
+import movement
 
-pilt = image()
-pilditootlusPall = imageProcess(50, 9999999, "ball")
+pilt = Image()
+pilditootlusPall = ImageProcess(50, 9999999, "ball")
 
 while True:
+    movement.setMovement(0,5,0,0)
     pilditootlusPall.find_objects(pilt.get_rbg_image())
     cords = pilditootlusPall.getcords()
     print(str(cords) + "ball")
