25cc53b601e21527aa0d8988246882c0c7fae8d6
urmo <u.schults@gmail.com>
2022-01-24 21:23:07 +0200

    main

--- saun/main.py
+++ saun/main.py
@@ -149,7 +149,7 @@ def align_basket():
         y_rotation = (500 - ball_coordinates[0][1]) / 10
         print("Ball: " + str(ball_coordinates) + " Basket: " + str(basket_coordinates))
         print("X: " + str(x_rotation) + " Y: " + str(y_rotation))
-        if basket_coordinates[0][0] < camera_x_mid+30 and basket_coordinates[0][0] > camera_x_mid-30 and ball_coordinates[0][0] < camera_x_mid+20 and ball_coordinates[0][0] > camera_x_mid-20:
+        if basket_coordinates[0][0] < camera_x_mid+70 and basket_coordinates[0][0] > camera_x_mid-70 and ball_coordinates[0][0] < camera_x_mid+20 and ball_coordinates[0][0] > camera_x_mid-20:
             basket_depth = image.getDepth(basket_coordinates[0][0], basket_coordinates[0][1])
             print("Basket distance: " + str(basket_depth))
             if basket_depth > 0.5:
