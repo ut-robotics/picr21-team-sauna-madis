28171b16b7e2a091cea22c8b73791862c8d6c5e9
urmo <u.schults@gmail.com>
2022-01-24 20:31:43 +0200

    fpeth

--- saun/main.py
+++ saun/main.py
@@ -159,7 +159,7 @@ def align_basket():
 
 
         elif basket_coordinates[0][0] > camera_x_mid-5:
-            movement.setMovement(180, 10, int(-(x_rotation + y_rotation)), 0)
+            movement.setMovement(180, 10, int(-x_rotation + y_rotation), 0)
         else:
             movement.setMovement(0, 10, int(x_rotation + y_rotation), 0)
 
