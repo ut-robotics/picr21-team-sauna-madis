0013c8dc647cfe93e3ad97b99157c6a7fd4e2fe0
urmo <u.schults@gmail.com>
2022-01-24 19:19:15 +0200

    controller

--- saun/main.py
+++ saun/main.py
@@ -132,8 +132,8 @@ def align_basket():
         if move_style_check(): return True
 
         ball_coordinates, basket_coordinates = get_ballNbasket_cord()
-        x_rotation = (ball_coordinates[0][0] - camera_x_mid) / - 33  # -4
-        y_rotation = (500 - ball_coordinates[0][1]) / 17
+        x_rotation = (ball_coordinates[0][0] - camera_x_mid) / -20  # -4
+        y_rotation = (500 - ball_coordinates[0][1]) / 15
         #print("Ball: " + str(ball_coordinates) + " Basket: " + str(basket_coordinates))
         #print("X: " + str(x_rotation) + " Y: " + str(y_rotation))
         if basket_coordinates[0][0] < camera_x_mid+5 and basket_coordinates[0][0] > camera_x_mid-5 and ball_coordinates[0][0] < camera_x_mid+5 and ball_coordinates[0][0] > camera_x_mid-5:
@@ -144,9 +144,9 @@ def align_basket():
                 break
 
         elif basket_coordinates[0][0] > camera_x_mid-5:
-            movement.setMovement(180, 7, int(-x_rotation - y_rotation), 0)
+            movement.setMovement(180, 10, int(-(x_rotation + y_rotation)), 0)
         else:
-            movement.setMovement(0, 7, int(x_rotation + y_rotation), 0)
+            movement.setMovement(0, 10, int(x_rotation + y_rotation), 0)
 
 
 def throw_ball(basket_depth):
