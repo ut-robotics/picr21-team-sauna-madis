ff5229feb4013945b0dae225386a91b09d1a1b2a
urmo <u.schults@gmail.com>
2022-01-24 15:20:34 +0200

    proc

--- saun/imageProcess.py
+++ saun/imageProcess.py
@@ -106,7 +106,7 @@ class ImageProcess:
         #Show images
         
         cv2.namedWindow("Processed image", cv2.WINDOW_AUTOSIZE)
-        cv2.imshow("Processed image", outimage)
+        cv2.imshow("Processed image", thresholded)
         cv2.waitKey(1)
         #self.show_image(window, self.hsv)
 
