00779e198dcd6dd6fe8157f6b6a7b66217ff9992
urmo <u.schults@gmail.com>
2022-01-24 17:43:12 +0200

    controller

--- saun/ps4controller.py
+++ saun/ps4controller.py
@@ -69,7 +69,7 @@ class controller:
             def on_L1_release(self):
                 self.movement.stop()
 
-        controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False, gamestate=MoveStyle.AUTO)
+        controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
         # you can start listening before controller is paired, as long as you pair it within the timeout window
         controller.listen(timeout=60)
 
